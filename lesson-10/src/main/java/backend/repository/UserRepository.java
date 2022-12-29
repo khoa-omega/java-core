7d8d8e901ea34f8d2892d4608260a92ebaab2c42
khoa-omega <nvkhoa05@gmail.com>
2022-12-29 19:29:29 +0700

    Update

--- lesson-10/src/main/java/backend/repository/UserRepository.java
+++ lesson-10/src/main/java/backend/repository/UserRepository.java
@@ -44,4 +44,29 @@ public class UserRepository {
             pStmt.executeUpdate();
         }
     }
+
+    public int update(User user) throws SQLException {
+        String sql = "UPDATE `user` SET `name` = ?, email = ?, `password` = ? WHERE id = ?";
+        try (
+                Connection connection = JDBCUtils.getConnection();
+                PreparedStatement pStmt = connection.prepareStatement(sql)
+        ) {
+            pStmt.setString(1, user.getName());
+            pStmt.setString(2, user.getEmail());
+            pStmt.setString(3, user.getPassword());
+            pStmt.setInt(4, user.getId());
+            return pStmt.executeUpdate();
+        }
+    }
+
+    public int deleteById(int id) throws SQLException {
+        String sql = "DELETE FROM `user` WHERE id = ?";
+        try (
+                Connection connection = JDBCUtils.getConnection();
+                PreparedStatement pStmt = connection.prepareStatement(sql)
+        ) {
+            pStmt.setInt(1, id);
+            return pStmt.executeUpdate();
+        }
+    }
 }
